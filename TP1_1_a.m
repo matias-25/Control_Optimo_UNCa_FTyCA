0ce49e81a30a3962aca497de332416a2f481a985
Patricio Arévalo <89281750+matias-25@users.noreply.github.com>
2025-04-08 00:14:06 -0300

    Add files via upload

---
+++ TP1_1_a.m
@@ -0,0 +1,106 @@
+clear all;
+%sistema a lazo abierto
+A=[-2  2;
+    2 -5];
+B=[1;0];
+C=[0 1];
+D=0;
+[num , den]= ss2tf(A,B,C,D);
+sys= tf(num,den);
+f=step(sys);
+h=0.01;
+f1 = diff(f);%derivada primera
+f2 = diff(f1);%derivada segunda
+p_inflex_t=0;
+p_inflex=0;
+pend_tg=0;
+t=0:h:(length(f)*h)-h;
+
+%punto de inflexion
+for i=1:length(f)-2
+    if i<length(f)-2;
+    if (f2(i)>0 && f2(i+1)<0) || (f2(i)<0 && f2(i+1)>0)
+        p_inflex_t=t(i);
+        p_inflex=f(i);
+        pend_tg=(f(i)-f(i-1))/(t(i)-t(i-1));
+    end
+    end
+end
+rec_tg=pend_tg*(t-p_inflex_t)+p_inflex;%recta tangente al punto de inflexion
+
+figure(1);
+plot(t,f);title('Planta a Lazo Abierto');xlabel('tiempo[seg]');axis([0 5 -0.2 1.5]);grid on; hold on;
+plot(p_inflex_t,p_inflex,'O');hold on
+plot(t,rec_tg,'r');
+
+%obtencion de L y T_ para calcular los parámetros del controlador PID
+%con el primer metodo de Ziegler-Nichols
+for i=1:length(f)
+    if 0.995<rec_tg(i) && 1.005>rec_tg(i)
+        T_=t(i);
+    end
+    if min(abs(rec_tg(i)))<=0.005 
+       L=t(i); 
+    end
+end
+
+%Las constantes PID
+Kp=1.2*T_/L;
+Ti=2*L;Ki=Kp/Ti;
+Td=0.5*L;Kd=Kp*Td;
+
+%PID
+A1=((2*Kp*h)+(Ki*(h^2))+(2*Kd))/(2*h);
+B1=(-2*Kp*h+Ki*(h^2)-4*Kd)/(2*h);
+C1=Kd/h;
+e=zeros(length(f),1);%error
+u=0;
+X=-[0;0];
+i=0;k=0;
+referencia=1; %%altura en metros del tanque 2
+for j=0:h:(length(f)*h)-h;
+  i=i+1;
+  k=i+2;
+  e(k)=referencia - X(2); %ERROR
+  u=u+A1*e(k)+B1*e(k-1)+C1*e(k-2); %PID
+  accion(i)=u;
+  h2(i)=X(2);
+  for jj=0:h
+    X_P=A*X+B*u;
+    X=X+h*X_P;
+  end
+end
+
+%constantes PID, con ajuste fino
+Kp=3;Ki=3;Kd=0.01;
+%PID
+A1=((2*Kp*h)+(Ki*(h^2))+(2*Kd))/(2*h);
+B1=(-2*Kp*h+Ki*(h^2)-4*Kd)/(2*h);
+C1=Kd/h;
+e=zeros(length(f),1);%error
+u=0;
+X=-[0;0];
+i=0;k=0;
+referencia=1; %%altura en metros del tanque 2
+for j=0:h:(length(f)*h)-h;
+  i=i+1;
+  k=i+2;
+  e(k)=referencia - X(2); %ERROR
+  u=u+A1*e(k)+B1*e(k-1)+C1*e(k-2); %PID
+  accion2(i)=u;
+  h2_2(i)=X(2);
+  for jj=0:h
+    X_P=A*X+B*u;
+    X=X+h*X_P;
+  end
+end
+
+figure(2);
+subplot(2,1,1);
+plot(t,accion,'b');
+title('Accion de control');xlabel('tiempo[seg]');grid on;hold on;
+plot(t,accion2,'r');legend('calculado','con ajuste');
+subplot(2,1,2);
+plot(t,h2,'b');
+title('Salida del sistema');xlabel('tiempo[seg]');grid on;hold on;
+plot(t,h2_2,'r');legend('calculado','con ajuste');
---
+++ TP1_1_b.m
@@ -0,0 +1,66 @@
+clear all;
+%sistema a lazo abierto
+A=[-2  2;2 -5];
+B=[1;0];
+C=[0 1];
+D=0;
+figure(1);
+sys=ss(A,B,C,D);
+rlocus(sys);
+
+%obtension de la ecuacion caraterística
+%|sI-A|=[s 0;  -  [-2  2;
+    %    0 s]       2 -5]
+%|sI-A|= |s+2 -2; -2 s+5|=(s+2)(s+5)-(-2)*(-2)=s^2+5s+2s-4+10=s^2+7s+6  
+%p=poly(A);
+%matriz de polos deseados
+J=[-6+1i -6-1i];
+
+%Matriz de controlabilidad
+%M=ctrb(A,B);
+%det_M=det(M);
+
+%Diseño mediante la Fórmula de Ackermann
+Kack=acker(A,B,J);
+K=place(A,B,J)
+
+% Calcular la matriz del sistema en lazo cerrado
+Acl = A - B * K;
+
+% Calcular los polos del sistema en lazo cerrado
+poles = eig(Acl);
+h=1/(10*max(abs(poles)));
+%Diseño control óptimo en tiempo continuo con un integrador en el lazo para 
+%referencia distinta de cero
+tiempo=10/h;
+t=0:h:tiempo*h;
+X=[0 ; 0];
+A_amp=[A zeros(2,1);-C 0];
+B_amp=[B;0];
+C_amp=[0 1 0];
+%J_amp=[-6+1i -6-1i -1];color='b';
+J_amp=[-6+1i -6-1i -10];color='r';
+K_amp=place(A_amp,B_amp,J_amp)
+i=0;
+ref=1; %%altura en metros del tanque 2
+psi=0; %Error integrado
+
+while(i<(tiempo))
+  
+  u=-K_amp*([X;psi]);%ley de control
+  X_P=A*X+B*u;%X punto
+  X=X+h*X_P;%Esto es el cálculo de la integral como sumatoria
+  psi=psi+h*(ref-C*X);
+  
+  i=i+1;
+  h1(i)=X(1);
+  h2(i)=X(2);
+  accion(i)=u;
+end
+
+figure(2);
+subplot(2,1,1);
+plot(t,h2,color); title('Salida del sistema');xlabel('tiempo[seg]');grid on; hold on;
+%step(sys,'r'), legend('altura controlada','altura sin controlador');
+subplot(2,1,2) ;
+plot(t,accion,color); title('Accion de control "caudal"');xlabel('tiempo[seg]');grid on;hold on;
