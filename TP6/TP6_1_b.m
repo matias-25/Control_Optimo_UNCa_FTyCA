54a96aaf48b294a8fb559d724c99870b9bb24bd7
Patricio Arévalo <89281750+matias-25@users.noreply.github.com>
2025-06-12 00:09:27 -0300

    Add files via upload

--- TP6/TP6_1_b.m
+++ TP6/TP6_1_b.m
@@ -1,26 +1,30 @@
 clear all;%close all; 
 %% Constantes del sistema
-m=.1;Fricc=0.1; long=0.6;g=9.8;M=.5; 
+% w= , a= , b= , c=velocidad del avion
+w=2;a=0.05; b=5;c=80;
 
 %Condiciones iniciales
-d(1)=0;
-d_p(1)=0;
-fi(1)=pi/3;
-fi_p(1)=0;
+alfa(1)=0;
+fhi(1)=0;
+fhi_p(1)=0;
+h(1)=100;
 u(1)=0; %accion de control 
-x= [-d(1);-d_p(1);fi(1);fi_p(1)];
-x0=x;
+x= [-alfa(1);-fhi(1);-fhi_p(1);-h(1)];
+x0=-x;
 
-%Versión linealizada en el equilibrio inestable. Sontag Pp 104. 
-Mat_Ac=[0 1 0 0;0 -Fricc/M -m*g/M 0;0 0 0 1;0 Fricc/(long*M) g*(m+M)/(long*M) 0];
-Mat_Bc=[0; 1/M; 0; -1/(long*M)];
-Mat_Cc=[0 0 1 0; 1 0 0 0];
+%Versión linealizada del avion x=[alfa;fi;fi_p;h]
+%alfa= direccion de vuelo, fi= angulo cabeceo (piloto) , 
+%fi_p = velocidad angulo cabeceo, h= altura
+Mat_Ac=[-a a 0 0;0 0 1 0; w^2 -w^2 0 0; c 0 0 0];
+Mat_Bc=[0; 0; b*w^2; 0];
+Mat_Cc=[0 0 0 1; %mido la altura
+        0 1 0 0];   %mido angulo cabeceo
 
 %% Sistema continuo
 sys_c=ss(Mat_Ac,Mat_Bc,Mat_Cc,0); 
 
 %% Sistema discreto
-fs=100; %frecuencia de muestreo
+fs=500; %frecuencia de muestreo
 Ts=1/fs;
 
 sys_d=c2d(sys_c,Ts,'zoh'); 
@@ -29,7 +33,7 @@ Mat_Bd=sys_d.b;
 Mat_Cd=sys_d.c;
 
 %% controlador DLQG
-Q=diag([1e2 1e1 1e6 1e1]);R=1e0;%Matrices de diseño del controlador DLQG
+Q=diag([1e2 1e2 1e0 5e-4]);R=1e0;%Matrices de diseño del controlador DLQG
 Kx = dlqr(Mat_Ad,Mat_Bd,Q,R); %ganacia del controlador
 
 %% Obsevador de Luenberger
@@ -37,19 +41,19 @@ Kx = dlqr(Mat_Ad,Mat_Bd,Q,R); %ganacia del controlador
 A_o=Mat_Ad'; 
 B_o=Mat_Cd'; 
 C_o=Mat_Bd'; 
-Qo=diag([1e0 1e0 1e-1 1e0]);Ro=diag([1e1, 1e1]); 
+Qo=diag([1e-3 1e-3 1e-2 1e0]);Ro=diag([1e0, 1e0]); 
 Ko= dlqr(A_o,B_o,Qo,Ro); %ganancia del observador
 %%
 Qcomp=eye(4);%Para comparar el desempeño de los controladores
 %% Monte Carlo
 % Consigna 0, 0.01, 0.02, 0.05 y 0.1
-sQ=0; %Para F
-sR=0; %Para G. Covarianza del ruido de medicion sigma=sqrt(sR)
+sQ=0.01; %Para F
+sR=0.01; %Para G. Covarianza del ruido de medicion sigma=sqrt(sR)
 F_=sQ*eye(4); %Covarianza del ruido de estado Sigma=sqrt(sQ)
 G_=sR;
 S=Q;
 P=S; %condición inicial de P
-kmax=2000;
+kmax=20000;
 Realizaciones=5; %Cantidad de realizaciones para el Monte Carlo.
 %Kx=zeros(kmax,4);
 Kv=zeros(kmax,4);
@@ -74,10 +78,10 @@ for trial=1:Realizaciones %Empieza el Monte Carlo
     w=randn(2,kmax);
     x=x0+F_*v(:,1);
     x_hat=[0;0;0;0];% variables estado observador 
-    d(trial,1)=x(1);
-    d_p(trial,1)=x(2);
-    fi(trial,1)=x(3);
-    fi_p(trial,1)=x(4);
+    alfa(trial,1)=x(1);
+    fhi(trial,1)=x(2);
+    fhi_p(trial,1)=x(3);
+    h(trial,1)=x(4);
     
     for ki=1:kmax-1
         %u(trial,ki)=-Kx(1,:)*x; %DLQR
@@ -87,29 +91,29 @@ for trial=1:Realizaciones %Empieza el Monte Carlo
         %y_sal(trial,ki)=Mat_Cd*x+G_*w(ki);
         Ys=Mat_Cd*x + G_*w(:,ki);
         y_sal(trial,ki)=Ys(1);
-        x=mopdm2(Ts,x,u(trial,ki))+F_*v(:,ki+1);
+        x=modelo_avion(Ts,x,u(trial,ki))+F_*v(:,ki+1);
         x_hat= Mat_Ad*x_hat + Mat_Bd*u(trial,ki) + Ko'*(Ys-Mat_Cd*x_hat);% Obsevador de Luenberger
-        d(trial,ki+1)=x(1);
-        d_p(trial,ki+1)=x(2);
-        fi(trial,ki+1)=x(3);
-        fi_p(trial,ki+1)=x(4);
+        alfa(trial,ki+1)=x(1);
+        fhi(trial,ki+1)=x(2);
+        fhi_p(trial,ki+1)=x(3);
+        h(trial,ki+1)=x(4);
     end
     Jn_(trial,ki+1)=Jn_(trial,ki+1)+x'*S*x;
     u(trial,ki+1)=-Kx(1,:)*[x]-Kv(1,:)*[v(:,ki)];
 end
 
-Jn=mean(Jn_);disp(['El valor de costo es Jn(end)=' num2str(Jn(end)) '.fi(1)=' num2str(fi(1)) '.']);
+Jn=mean(Jn_);disp(['El valor de costo es Jn(end)=' num2str(Jn(end)) '.fi(1)=' num2str(fhi_p(1)) '.']);
 %% Graficos
 t=t*Ts;
 TamanioFuente=14;
 figure;
-subplot(3,2,2);hold on;grid on;title('Ángulo \phi','FontSize',TamanioFuente);
-plot(t,fi,'*');
-subplot(3,2,4);hold on;grid on; title('Velocidad ángulo \phi_p','FontSize',TamanioFuente);
-plot(t,fi_p,'*');
-subplot(3,2,1);hold on; grid on;title('Posición carro \delta','FontSize',TamanioFuente);hold on;
-plot(t,d,'*');
-subplot(3,2,3);hold on; grid on;title('Velocidad carro \delta_p','FontSize',TamanioFuente);hold on;
-plot(t,d_p,'*');
+subplot(3,2,1);hold on;grid on; title('Altura avion h','FontSize',TamanioFuente);
+plot(t,h,'*');
+subplot(3,2,2);hold on; grid on;title('Ángulo cabeceo \phi','FontSize',TamanioFuente);hold on;
+plot(t,fhi,'*');
+subplot(3,2,4);hold on;grid on;title('Velocidad Ángulo cabeceo\phi_p','FontSize',TamanioFuente);
+plot(t,fhi_p,'*');
+subplot(3,2,3);hold on; grid on;title('Ángulo de vuelo \alpha','FontSize',TamanioFuente);hold on;
+plot(t,alfa,'*');
 subplot(3,1,3); grid on;title('Acción de control','FontSize',TamanioFuente);xlabel('Tiempo en Seg.','FontSize',TamanioFuente);hold on;
 plot(t,u,'*');
